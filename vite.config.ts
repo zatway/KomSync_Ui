0d151fc913e1f17c43590237db8ace67efd27c1d
zatway <radmirar00@mail.ru>
2026-03-10 21:34:12 +0500

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
                 plugins: [['babel-plugin-react-compiler'],],
             },
         }),
-        tailwindcss()
+        tailwindcss(),
+        tsconfigPaths(),
     ],
     resolve: {
         alias: {
